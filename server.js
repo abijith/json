aeefbaa7201ff6ec6300c5a224422363908c7f9e
Abijith K <abijith973@gmail.com>
2019-08-12 14:54:12 +0530

    adding text

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+    res.end('Just setting up now..');
 }).listen(3000);
